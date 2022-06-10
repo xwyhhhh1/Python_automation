ef9c6ddae6f4712a00f3677fe8719d6aacf5d95c
xwyhhhh1 <2293558627@qq.com>
2022-06-10 17:47:38 +0800

    update

--- cs/cs/Django_test/fist.py
+++ cs/cs/Django_test/fist.py
@@ -1,5 +1,11 @@
 import os
-def test():
-    ds=os.system('netstat -a')
-
-test()
+#public variable
+MYSQL_URL = "https://mirrors.tuna.tsinghua.edu.cn/mysql/downloads/MySQL-5.7/mysql-5.7.38-linux-glibc2.12-x86_64.tar.gz"
+DOWLOAD_MYSQL_CMD = "wget" + '\t' + MYSQL_URL + '\t' + "--no-check-certificate"
+def install():
+    de=os.system("netstat -a")
+    dc=os.system(DOWLOAD_MYSQL_CMD)
+    #ds=os.uname()
+    return dc
+if __name__ == '__main__':
+    install()
